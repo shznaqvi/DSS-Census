a1f7179aca10a86050866699959080b39de16fe2
gul.sanober <gulsanobermemon@gmail.com>
2017-01-13 11:55:03 +0500

    Section D, E, F, I, J Layouts completed

--- app/src/main/java/edu/aku/hassannaqvi/dss_census/SectionDActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/dss_census/SectionDActivity.java
@@ -1,8 +1,17 @@
 package edu.aku.hassannaqvi.dss_census;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by gul.sanober on 1/12/2017.
  */
 
-public class SectionDActivity {
+public class SectionDActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_d);
+    }
 }
