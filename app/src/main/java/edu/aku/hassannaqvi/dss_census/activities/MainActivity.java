d469048e92d8c33e36a2f5fc04ff2a438049116b
gul.sanober <gulsanobermemon@gmail.com>
2017-04-14 12:35:47 +0500

    Section B activity changes

--- app/src/main/java/edu/aku/hassannaqvi/dss_census/activities/MainActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/dss_census/activities/MainActivity.java
@@ -104,7 +104,7 @@ public class MainActivity extends Activity {
     }
 
     public void openForm(View v) {
-        Intent oF = new Intent(MainActivity.this, SectionBActivity.class);
+        Intent oF = new Intent(MainActivity.this, SectionAActivity.class);
         startActivity(oF);
     }
 
