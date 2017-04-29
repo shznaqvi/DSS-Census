d9d6b438f1ec941cd9e203f4b4ee3568da3de94a
ali.azaz <openmind_ali@hotmail.com>
2017-04-29 11:09:04 +0500

    fix bug sec M

--- app/src/main/java/edu/aku/hassannaqvi/dss_census/activities/SectionMActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/dss_census/activities/SectionMActivity.java
@@ -23,6 +23,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import edu.aku.hassannaqvi.dss_census.R;
 import edu.aku.hassannaqvi.dss_census.core.DatabaseHelper;
+import edu.aku.hassannaqvi.dss_census.core.MainApp;
 
 public class SectionMActivity extends Activity {
 
@@ -628,7 +629,7 @@ public class SectionMActivity extends Activity {
         sM.put("dcm19", dcm1901.isChecked() ? "1" : dcm1902.isChecked() ? "2" : dcm1999.isChecked() ? "99" : "0");
         sM.put("dcm20", dcm2001.isChecked() ? "1" : dcm2002.isChecked() ? "2" : dcm2099.isChecked() ? "99" : "0");
 
-        //DCEApp.fc.setROW_Se(String.valueOf(se));
+        MainApp.fc.setsM(String.valueOf(sM));
 
         Toast.makeText(this, "Validation Successful! - Saving Draft...", Toast.LENGTH_SHORT).show();
     }
