4c5f18d74743ffd285731c3b60e58787bb3b06f0
ali.azaz <openmind_ali@hotmail.com>
2017-06-16 09:49:29 +0500

    commit

--- app/src/main/java/edu/aku/hassannaqvi/dss_census/core/MainApp.java
+++ app/src/main/java/edu/aku/hassannaqvi/dss_census/core/MainApp.java
@@ -257,7 +257,7 @@ public class MainApp extends Application {
 
     protected void showCurrentLocation() {
 
-        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
         if (location != null) {
             String message = String.format(
