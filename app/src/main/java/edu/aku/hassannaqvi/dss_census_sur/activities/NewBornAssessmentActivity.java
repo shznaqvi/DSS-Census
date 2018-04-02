befffc913183819991202eb9db5106070463fd63
Ramsha Saeed <ramshasaeed2016@gmail.com>
2018-04-02 19:42:05 +0500

    newBorn Activity layout

--- app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/activities/NewBornAssessmentActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/activities/NewBornAssessmentActivity.java
@@ -1,23 +1,37 @@
 package edu.aku.hassannaqvi.dss_census_sur.activities;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.databinding.DataBindingUtil;
-
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
 
 import edu.aku.hassannaqvi.dss_census_sur.R;
+import edu.aku.hassannaqvi.dss_census_sur.databinding.ActivityNewBornAssessmentBinding;
 
 public class NewBornAssessmentActivity extends Activity {
+    ActivityNewBornAssessmentBinding bi;
 
-public class NewBornAssessmentActivity extends AppCompatActivity {
-ActivityNewBornAssessmentBinding bi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bi = DataBindingUtil.setContentView(this, R.layout.activity_new_born_assessment);
-        bi.setCallBack(this);
-       // setContentView(R.layout.activity_new_born_assessment);
+        bi.setCallback(this);
+        // setContentView(R.layout.activity_new_born_assessment);
+        bi.dnb25.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == R.id.dnb25b) {
+                    bi.fldGrpdnb25.setVisibility(View.GONE);
+                    bi.dnb26.clearCheck();
+                    bi.dnb26x.setText(null);
+                    bi.dnb27.clearCheck();
+                    bi.dnb28.setText(null);
+                } else {
+                    bi.fldGrpdnb25.setVisibility(View.VISIBLE);
+                }
+            }
 
+        });
     }
 }
