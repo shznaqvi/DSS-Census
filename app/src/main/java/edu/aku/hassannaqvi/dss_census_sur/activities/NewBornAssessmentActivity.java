d47f435d6c2519f4003ff6cb3a116f68a75bff58
Ramsha Saeed <ramshasaeed2016@gmail.com>
2018-04-02 19:01:47 +0500

    newBorn Activity layout

--- app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/activities/NewBornAssessmentActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/activities/NewBornAssessmentActivity.java
@@ -2,15 +2,19 @@ package edu.aku.hassannaqvi.dss_census_sur.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.databinding.DataBindingUtil;
+
 
 import edu.aku.hassannaqvi.dss_census_sur.R;
 
 public class NewBornAssessmentActivity extends AppCompatActivity {
-
+ActivityNewBornAssessmentBinding bi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_born_assessment);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_new_born_assessment);
+        bi.setCallBack(this);
+       // setContentView(R.layout.activity_new_born_assessment);
 
     }
 }
