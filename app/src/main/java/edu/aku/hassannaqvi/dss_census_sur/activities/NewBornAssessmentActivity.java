709681688477949166219184ab20495b7fc7b476
ali.azaz <openmind_ali@hotmail.com>
2018-04-02 17:30:50 +0500

    New DSS-CENSUS SUR: 2new forms add (Child Death)

--- app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/activities/NewBornAssessmentActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/activities/NewBornAssessmentActivity.java
@@ -1,11 +1,12 @@
 package edu.aku.hassannaqvi.dss_census_sur.activities;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import edu.aku.hassannaqvi.dss_census_sur.R;
 
-public class NewBornAssessmentActivity extends AppCompatActivity {
+public class NewBornAssessmentActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
