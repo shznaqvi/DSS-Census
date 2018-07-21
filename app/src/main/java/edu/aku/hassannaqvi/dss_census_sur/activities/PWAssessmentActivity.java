3207ef1b2c669a07d30ed5ee1f3e275b04276048
ali.azaz <ali.azaz.alam@hotmail.com>
2018-07-21 13:01:02 +0500

    PW assesment form add

---
+++ app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/activities/PWAssessmentActivity.java
@@ -0,0 +1,87 @@
+package edu.aku.hassannaqvi.dss_census_sur.activities;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import edu.aku.hassannaqvi.dss_census_sur.R;
+import edu.aku.hassannaqvi.dss_census_sur.core.DatabaseHelper;
+import edu.aku.hassannaqvi.dss_census_sur.databinding.ActivityPwAssessmentBinding;
+
+public class PWAssessmentActivity extends AppCompatActivity {
+    ActivityPwAssessmentBinding bi;
+
+    String dtToday = new SimpleDateFormat("dd-MM-yy HH:mm").format(new Date().getTime());
+
+    DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_pw_assessment);
+        bi.setCallback(this);
+
+        db = new DatabaseHelper(this);
+
+        setContentsUI();
+
+    }
+
+    public void setContentsUI() {
+
+
+    }
+
+    public void BtnEnd() {
+        Toast.makeText(this, "Starting Form Ending Section", Toast.LENGTH_SHORT).show();
+        /*finish();
+        startActivity(new Intent(this, NB_EndingActivity.class)
+                .putExtra("check", false));*/
+    }
+
+    public void BtnContinue() {
+
+        Toast.makeText(this, "Processing This Section", Toast.LENGTH_SHORT).show();
+        /*if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                Toast.makeText(this, "Starting Next Section", Toast.LENGTH_SHORT).show();
+
+                finish();
+                startActivity(new Intent(this, NB_EndingActivity.class)
+                        .putExtra("check", true));
+
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }*/
+    }
+
+    public boolean formValidation() {
+
+
+        return true;
+    }
+
+    public void SaveDraft() throws JSONException {
+
+
+    }
+
+
+    private boolean UpdateDB() {
+        return true;
+    }
+
+
+}
