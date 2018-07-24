49946960f012553ad8a6067154b93b941c7b3739
mustafa.ansari <mustufa_ayub@hotmail.com>
2018-07-24 16:44:34 +0500

    PW save draft done with complete layout

---
+++ app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/contracts/EventContract.java
@@ -0,0 +1,183 @@
+package edu.aku.hassannaqvi.dss_census_sur.contracts;
+
+import android.database.Cursor;
+import android.provider.BaseColumns;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+
+/**
+ * Created by hassan.naqvi on 11/30/2016.
+ */
+
+public class EventContract implements Serializable {
+
+    private String euid;
+    private String formdate;
+    private String name;
+    private String dss_id_member;
+    private String status;
+    private String lmp_dt;
+    private String status_date;
+    private String birth_time;
+    private String gender;
+
+    public EventContract() {
+    }
+
+    public EventContract(EventContract cc) {
+        this.euid = cc.euid;
+        this.formdate = cc.formdate;
+        this.dss_id_member = cc.dss_id_member;
+        this.name = cc.name;
+        this.status = cc.status;
+        this.lmp_dt = cc.lmp_dt;
+        this.status_date = cc.status_date;
+        this.birth_time = cc.birth_time;
+        this.gender = cc.gender;
+
+    }
+
+    public String getEuid() {
+        return euid;
+    }
+
+    public void setEuid(String euid) {
+        this.euid = euid;
+    }
+
+    public String getFormdate() {
+        return formdate;
+    }
+
+    public void setFormdate(String formdate) {
+        this.formdate = formdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDss_id_member() {
+        return dss_id_member;
+    }
+
+    public void setDss_id_member(String dss_id_member) {
+        this.dss_id_member = dss_id_member;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getLmp_dt() {
+        return lmp_dt;
+    }
+
+    public void setLmp_dt(String lmp_dt) {
+        this.lmp_dt = lmp_dt;
+    }
+
+    public String getStatus_date() {
+        return status_date;
+    }
+
+    public void setStatus_date(String status_date) {
+        this.status_date = status_date;
+    }
+
+    public String getBirth_time() {
+        return birth_time;
+    }
+
+    public void setBirth_time(String birth_time) {
+        this.birth_time = birth_time;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public EventContract Sync(JSONObject jsonObject) throws JSONException {
+        this.euid = jsonObject.getString(singleEV.COLUMN_EUID);
+        this.formdate = jsonObject.getString(singleEV.COLUMN_FORMDATE);
+        this.name = jsonObject.getString(singleEV.COLUMN_NAME);
+        this.dss_id_member = jsonObject.getString(singleEV.COLUMN_DSS_ID_MEMBER);
+        this.status = jsonObject.getString(singleEV.COLUMN_STATUS);
+        this.lmp_dt = jsonObject.getString(singleEV.COLUMN_LMP_DT);
+        this.status_date = jsonObject.getString(singleEV.COLUMN_STATUS_DATE);
+        this.birth_time = jsonObject.getString(singleEV.COLUMN_BIRTH_TIME);
+        this.gender = jsonObject.getString(singleEV.COLUMN_GENDER);
+
+
+        return this;
+
+    }
+
+    public EventContract Hydrate(Cursor cursor) {
+
+        this.euid = cursor.getString(cursor.getColumnIndex(singleEV.COLUMN_EUID));
+        this.formdate = cursor.getString(cursor.getColumnIndex(singleEV.COLUMN_FORMDATE));
+        this.name = cursor.getString(cursor.getColumnIndex(singleEV.COLUMN_NAME));
+        this.dss_id_member = cursor.getString(cursor.getColumnIndex(singleEV.COLUMN_DSS_ID_MEMBER));
+        this.status = cursor.getString(cursor.getColumnIndex(singleEV.COLUMN_STATUS));
+        this.lmp_dt = cursor.getString(cursor.getColumnIndex(singleEV.COLUMN_LMP_DT));
+        this.status_date = cursor.getString(cursor.getColumnIndex(singleEV.COLUMN_STATUS_DATE));
+        this.birth_time = cursor.getString(cursor.getColumnIndex(singleEV.COLUMN_BIRTH_TIME));
+        this.gender = cursor.getString(cursor.getColumnIndex(singleEV.COLUMN_GENDER));
+
+
+        return this;
+
+    }
+
+
+    public JSONObject toJSONObject() throws JSONException {
+
+        JSONObject json = new JSONObject();
+        json.put(singleEV.COLUMN_EUID, this.euid == null ? JSONObject.NULL : this.euid);
+        json.put(singleEV.COLUMN_FORMDATE, this.formdate == null ? JSONObject.NULL : this.formdate);
+        json.put(singleEV.COLUMN_NAME, this.name == null ? JSONObject.NULL : this.name);
+        json.put(singleEV.COLUMN_DSS_ID_MEMBER, this.dss_id_member == null ? JSONObject.NULL : this.dss_id_member);
+        json.put(singleEV.COLUMN_STATUS, this.status == null ? JSONObject.NULL : this.status);
+        json.put(singleEV.COLUMN_LMP_DT, this.lmp_dt == null ? JSONObject.NULL : this.lmp_dt);
+        json.put(singleEV.COLUMN_STATUS_DATE, this.status_date == null ? JSONObject.NULL : this.status_date);
+        json.put(singleEV.COLUMN_BIRTH_TIME, this.birth_time == null ? JSONObject.NULL : this.birth_time);
+        json.put(singleEV.COLUMN_GENDER, this.gender == null ? JSONObject.NULL : this.gender);
+
+        return json;
+    }
+
+    public static abstract class singleEV implements BaseColumns {
+
+        public static final String TABLE_NAME = "events";
+        public static final String COLUMN_NAME_NULLABLE = "NULLHACK";
+        public static final String COLUMN_EUID = "euid";
+        public static final String COLUMN_ID = "id";
+        public static final String COLUMN_FORMDATE = "formdate";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_DSS_ID_MEMBER = "dss_id_member";
+        public static final String COLUMN_STATUS = "status";
+        public static final String COLUMN_LMP_DT = "lmp_dt";
+        public static final String COLUMN_STATUS_DATE = "status_date";
+        public static final String COLUMN_BIRTH_TIME = "birth_time";
+        public static final String COLUMN_GENDER = "gender";
+
+
+        public static String _URL = "events.php";
+    }
+}
