d057e3e9231ec422e2c484f4fefbb435dac5d3ee
ali.azaz <ali.azaz.alam@hotmail.com>
2018-07-27 12:03:10 +0500

    fixed bug

--- app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/activities/NB_EndingActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/activities/NB_EndingActivity.java
@@ -9,8 +9,6 @@ import android.widget.RadioGroup;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
-import org.json.JSONException;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -53,11 +51,7 @@ public class NB_EndingActivity extends Activity {
 
 //        Toast.makeText(this, "Processing This Section", Toast.LENGTH_SHORT).show();
         if (formValidation()) {
-            try {
-                SaveDraft();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            SaveDraft();
             if (UpdateDB()) {
 
                 finish();
