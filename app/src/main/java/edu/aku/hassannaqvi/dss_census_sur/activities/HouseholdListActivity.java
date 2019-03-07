47a92d21ce0d28a519bade7a88e39b023d09999b
ali.azaz <ali.azaz.alam@hotmail.com>
2019-03-07 18:07:48 +0500

    commit

--- app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/activities/HouseholdListActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/dss_census_sur/activities/HouseholdListActivity.java
@@ -71,6 +71,7 @@ public class HouseholdListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_household_list);
         ButterKnife.bind(this);
 
